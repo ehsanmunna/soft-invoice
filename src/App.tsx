4e0f257a4c7f66f9c0684bb22a5f5694d768a1c7
ehsan munna <grmunnabd@gmail.com>
2022-02-17 22:51:39 +0600

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
 import NotFound from './pages/NotFound/NotFound';
@@ -14,7 +12,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/reg" element={<Registration />} />
           <Route path="/*" element={<NotFound/>} />
-        </Routes>
+        </Routes> 
       </BrowserRouter>
     </div >
   );
