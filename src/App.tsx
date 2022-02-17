f7687f509ebde6b418eee321106fc41623077229
ehsan munna <grmunnabd@gmail.com>
2022-02-17 22:50:49 +0600

    not found added with route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/reg" element={<Registration />} />
+          <Route path="/*" element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
     </div >
