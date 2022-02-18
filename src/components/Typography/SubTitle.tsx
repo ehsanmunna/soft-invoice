b10b1740d7d857d64db2577d7d889de28a3493b7
ehsan munna <grmunnabd@gmail.com>
2022-02-18 10:36:16 +0600

    typography component file added

---
+++ src/components/Typography/SubTitle.tsx
@@ -0,0 +1,6 @@
+import { ITitle } from "../../interface/Title";
+
+
+const AppSubTitle = ({ name }: ITitle) => <h2>{name}</h2>;
+
+export default AppSubTitle
---
+++ src/components/Typography/Title.tsx
@@ -0,0 +1,6 @@
+import { ITitle } from "../../interface/Title";
+
+
+const AppTitle = ({ name }: ITitle) => <h1>{name}</h1>;
+
+export default AppTitle
