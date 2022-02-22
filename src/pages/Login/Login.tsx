5dc9893b0795246c8c2988cbd5a56fa902d7a4fa
ehsan munna <grmunnabd@gmail.com>
2022-02-22 23:40:29 +0600

    form layout added

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,36 +1,41 @@
+import { Box } from "@mui/material";
 import AppButtonPrimary from "../../components/Actions/PrimaryButton";
 import AppLink from "../../components/Common/Link";
 import AppTextInput from "../../components/FormElement/TextInput";
 import AppTextPassword from "../../components/FormElement/TextPassword";
+import FormContainer from "../../components/Layout/FormContainer";
 import AppLogo from "../../components/Media/Logo";
 import AppTitle from "../../components/Typography/Title";
 
+const padding = 10;
+
 function LoginPage() {
-    return (
-      <>
-        <div>
-          <AppLogo src="" alt="Logo"/>
-        </div>
-        <div>
-          <AppTitle name="Login to your account"/>
-        </div>
-        <div>
-          <form>
-            <div>
-            <AppTextInput placeholder="email/phone"/>
-            </div>
-            <div>
-            <AppTextPassword placeholder="password"/>
-            </div>
-          </form>
-          <AppButtonPrimary btnText="Login"/>
-        </div>
-        <div>
-          <p>Not registared? please <AppLink href="#" linkText="click here"/></p>
-        </div>
-      </>
-    );
-  }
-  
-  export default LoginPage;
-  
+  return (
+    <>
+      <div>
+        <AppLogo src="" alt="Logo" />
+      </div>
+      <div>
+        <AppTitle name="Login to your account" />
+      </div>
+      <FormContainer
+        formBody={(
+          <>
+            <Box style={{ padding: padding }}>
+              <AppTextInput placeholder="email/phone" />
+            </Box>
+            <Box style={{ padding: padding }}>
+              <AppTextPassword placeholder="password" />
+            </Box>
+          </>
+        )}
+        buttonText="Login"
+        formExtra={(
+          <p>Not registared? please <AppLink href="#" linkText="click here" /></p>
+        )}
+      />
+    </>
+  );
+}
+
+export default LoginPage;
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -3,6 +3,7 @@ import AppButtonPrimary from "../../components/Actions/PrimaryButton";
 import AppLink from "../../components/Common/Link";
 import AppTextInput from "../../components/FormElement/TextInput";
 import AppTextPassword from "../../components/FormElement/TextPassword";
+import FormContainer from "../../components/Layout/FormContainer";
 import AppLogo from "../../components/Media/Logo";
 import AppTitle from "../../components/Typography/Title";
 
@@ -17,17 +18,15 @@ function Registration() {
       <div>
         <AppTitle name="Be a member" />
       </div>
-      <Container maxWidth="sm">
-        
       
-
-      <div>
-        <form>
+      <FormContainer
+        formBody={(
+          <>
           <Box style={{ padding: padding }}>
             <AppTextInput placeholder="first name" />
           </Box>
           <Box style={{ padding: padding }}>
-              <AppTextInput placeholder="last name" />
+          <AppTextInput placeholder="last name" />
           </Box>
           <Box style={{ padding: padding }}>
             <AppTextInput placeholder="email/phone" />
@@ -38,13 +37,13 @@ function Registration() {
           <Box style={{ padding: padding }}>
             <AppTextPassword placeholder="password confirm" />
           </Box>
-        </form>
-        <AppButtonPrimary btnText="Create Account" />
-      </div>
-      <div>
-        <p>already registared? please <AppLink href="#" linkText="login" /></p>
-      </div>
-      </Container>
+          </>
+        )}
+        buttonText="Create Account"
+        formExtra={(
+          <p>already registared? please <AppLink href="#" linkText="login" /></p>
+        )}
+      />
     </>
   );
 }
